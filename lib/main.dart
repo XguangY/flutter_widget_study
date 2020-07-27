dc259505002348ee804f78073ec7d689adfa90a2
theone <xuguangyue@dtrobo.com>
2020-07-27 16:47:41 +0800

    Image Widget Test

--- lib/main.dart
+++ lib/main.dart
@@ -30,32 +30,60 @@ class App extends StatelessWidget {
           //         decorationStyle: TextDecorationStyle.double))
 
           //  Container 相当于div
+          // child: Container(
+          //   child: Text(
+          //     '小朋友??? 多',
+          //     style: TextStyle(fontSize: 45.0),
+          //   ),
+          //   // bottomCenter:下部居中对齐。
+          //   // botomLeft: 下部左对齐。
+          //   // bottomRight：下部右对齐。
+          //   // center：纵横双向居中对齐。
+          //   // centerLeft：纵向居中横向居左对齐。
+          //   // centerRight：纵向居中横向居右对齐。
+          //   // topLeft：顶部左侧对齐。
+          //   // topCenter：顶部居中对齐。
+          //   // topRight： 顶部居左对齐。
+          //   alignment: Alignment.topLeft,
+          //   width: 500.0,
+          //   height: 200.0,
+          //   // color: Colors.pink,
+          //   padding: const EdgeInsets.fromLTRB(10.0, 40.0, 0.0, 10.0),
+          //   margin: const EdgeInsets.all(10.0),
+          //   decoration: new BoxDecoration(
+          //     gradient: const LinearGradient(
+          //       colors: [Colors.pink, Colors.blue, Colors.red],
+          //     ),
+          //     border: Border.all(width: 2.0, color: Colors.black),
+          //   ), // 设置背景和边框 与color属性互斥
+          // ),
+
           child: Container(
-            child: Text(
-              '小朋友??? 多',
-              style: TextStyle(fontSize: 45.0),
+            // Image.asset:加载资源图片，就是加载项目资源目录中的图片,加入图片后会增大打包的包体体积，用的是相对路径。
+            // Image.network:网络资源图片，意思就是你需要加入一段http://xxxx.xxx的这样的网络路径地址。
+            // Image.file:加载本地图片，就是加载本地文件中的图片，这个是一个绝对路径，跟包体无关。
+            // Image.memory: 加载Uint8List资源图片,这个我目前用的不是很多，所以没什么发言权
+            child: new Image.network(
+              'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1595847907602&di=0bc9b1964f2608b842558cb44f2584bf&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fforum%2Fw%3D580%2Fsign%3D976d3e62e1fe9925cb0c695804a95ee4%2Fcec1913df8dcd10017e5de86738b4710b9122f26.jpg',
+              // BoxFit.fill:全图显示，图片会被拉伸，并充满父容器。
+              // BoxFit.contain:全图显示，显示原比例，可能会有空隙。
+              // BoxFit.cover：显示可能拉伸，可能裁切，充满（图片要充满整个容器，还不变形）。
+              // BoxFit.fitWidth：宽度充满（横向充满），显示可能拉伸，可能裁切。
+              // BoxFit.fitHeight ：高度充满（竖向充满）,显示可能拉伸，可能裁切。
+              // BoxFit.scaleDown：效果和contain差不多，但是此属性不允许显示超过源图片大小，可小不可大。
+              // BoxFit.none
+              // fit: BoxFit.cover,
+
+              // 图片混合模式（colorBlendMode）和color属性配合使用，能让图片改变颜色, 可以理解为滤镜
+              // color: Colors.red,
+              // colorBlendMode: BlendMode.darken,
+
+              // 图片重复
+              repeat: ImageRepeat.repeat,
             ),
-            // bottomCenter:下部居中对齐。
-            // botomLeft: 下部左对齐。
-            // bottomRight：下部右对齐。
-            // center：纵横双向居中对齐。
-            // centerLeft：纵向居中横向居左对齐。
-            // centerRight：纵向居中横向居右对齐。
-            // topLeft：顶部左侧对齐。
-            // topCenter：顶部居中对齐。
-            // topRight： 顶部居左对齐。
-            alignment: Alignment.topLeft,
-            width: 500.0,
+            width: 300.0,
             height: 200.0,
-            // color: Colors.pink,
-            padding: const EdgeInsets.fromLTRB(10.0, 40.0, 0.0, 10.0),
-            margin: const EdgeInsets.all(10.0),
-            decoration: new BoxDecoration(
-              gradient: const LinearGradient(
-                colors: [Colors.pink, Colors.blue, Colors.red],
-              ),
-              border: Border.all(width: 2.0, color: Colors.black),
-            ), // 设置背景和边框 与color属性互斥
+            color: Colors.lightBlue,
           ),
         ),
       ),
